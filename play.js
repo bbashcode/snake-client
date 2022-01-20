3d688fc207b3c72be86357a285396fbc605f20a7
bbashcode <binbash.code@gmail.com>
2022-01-20 01:48:07 +0000

    Input Module: refactoring in play.js and added input.js

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const {connect} = require("./client");
-console.log(connect.connect);
-
-//setup interface to handle user input from stdin
-const setupInput = function(){
- const stdin = process.stdin;
- stdin.setRawMode(true);
- stdin.setEncoding("utf8");
- stdin.resume();
- stdin.on("data", handleUserInput);
- return stdin;
-};
-
-const handleUserInput = function (input){
-  if (input === '\u0003') {
-    process.exit();   
-  }
-};
-
+const {setupInput} = require("./input");
+console.log("Connecting ...");
+connect();
 setupInput();
